e4f375af535a6de85e070b69128a6a50fe43db48
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-04-13 09:19:40 -0300

    universities.models

--- universities/models.py
+++ universities/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 
+class University(models.Model):
+    """
+    University table
+    """
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+    cpnj = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class MajorField(models.Model):
+    """
+    Major field table - study field of a major, can be broad or specific
+    e.g., Engeneering, Science, Politics, Health, etc
+    """
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Major(models.Model):
+    """
+    Major table - undergraduation course
+    e.g., Computer Engeneering, Economics, Law, etc
+    """
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+    fields = models.ManyToManyField(MajorField, related_name="majors")
+
+    class Meta:
+        ordering = ["name"]
