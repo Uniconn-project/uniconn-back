2fb61f038ff3b6d7dd12456c57990ea660da0827
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-04-13 10:42:19 -0300

    migrations made

--- profiles/models.py
+++ profiles/models.py
@@ -13,8 +13,10 @@ class Student(models.Model):
     first_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
-    university = models.ForeignKey(University, on_delete=models.SET_NULL, related_name="students")
-    major = models.ForeignKey(Major, on_delete=models.SET_NULL, related_name="students")
+    university = models.ForeignKey(
+        University, on_delete=models.SET_NULL, related_name="students", blank=True, null=True
+    )
+    major = models.ForeignKey(Major, on_delete=models.SET_NULL, related_name="students", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
