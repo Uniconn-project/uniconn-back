9760e372a7ec12017a525b117e691744841acb18
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-04-13 08:56:52 -0300

    started app universities

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # uniconn-back
 Django rest API
+
+# SETUP
+python -m venv env
+env/Scripts/Activate.ps1
+
+pip install -r requirements.txt
+
+# After installing another package run
+pip freeze > requirements.txt
+
+# Run server
+python manage.py runserver
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Student(models.Model):
+    """
+    Student table
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="student")
+    photo = models.ImageField(default="profile_avatar.jpg", upload_to="profile_photos")
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    #university = models.ForeingKey(on_delete=models.SET_NULL, related_name="students")
+    #major = models.ForeingKey(on_delete=models.SET_NULL, related_name="students")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering: ['-created_at']
