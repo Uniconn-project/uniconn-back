ea38e8d8a92a6a9d923ab28a30d5287cf6b74c58
Saulo <duart.herrera@gmail.com>
2021-04-15 15:46:06 -0300

    Initializing tests, ask for orientation from Felipe

--- projects/tests/test_models.py
+++ projects/tests/test_models.py
@@ -4,19 +4,34 @@ import pytz
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 from django.test import TestCase
-from profiles.models import Mentor, Profile, Student
-from universities.models import MajorField, University
+from projects.models import Market, Project, Student
+
+## Students, Markets, impotar esses dois e criar instacias e adiciona istacia do projects
+## Importar o model do arquivo que ele foi criado
+
+# Create user in test
+# user = self.User.objects.create(username=username, password="secret")
 
 
 class TestProjects(TestCase):
-    """
-    Eu suponho que como no arquivo __ini__.pyi tem apenas a definição de pegar o model do user
-    e vendo o diagrama posso inferir que a tabela projects tem a relação com a tabela students
-    que por sua vez tem relação com a profile que por ultimo se relaciona com user.
-    Ontem falamos sobre o tal efeito cascata então suponho que se eu pego um modelo de user por
-    conseguinte estarei testando usando a tabela students dentro do projects.
-    Posso estar equivocado, devo declarar um getter pra pegar um student model no __init__.pyi?
-    """
-
     def setUp(self):
         self.User = get_user_model()
+
+    # Test instance of Project class (table)
+    """
+        Meu raciocínio aqui é que eu devo pelo menos preecher o instaciamento da tabela projects
+        que é a primeira que eu quero testar, perguntar pro Felipe se preencher os parâmetros do
+        construtor da mesma é algo válido ou se já existe uma abstração para isso.     
+    """
+
+    def test_model(self):
+        category = "marketing"
+        name = "somaSolutions"
+        description = "Solutions for marketing of great enterprises"
+        # Here come Students I'm must initialize a User then go to a Student instance
+        username = "Jaime"
+        password = "shhhh"
+        self.User.objects.create(username=password, password=password)
+        # Creating major
+        project = Project.objects.create()
+        self.assertIsInstance(project, Project)
