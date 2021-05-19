63c425d60476050be47a17a393b9c2e3069d4123
felipe-carvalho12 <felipebarrettocarvalho@gmail.com>
2021-05-19 10:58:46 -0300

    universities tests OK

--- projects/tests/test_urls.py
+++ projects/tests/test_urls.py
@@ -9,3 +9,6 @@ BASE_URL = "/api/projects/"
 class TestUrls(TestCase):
     def test_get_markets_name_list_url(self):
         self.assertEqual(resolve(BASE_URL + "get-markets-name-list").func, get_markets_name_list)
+
+    def test_get_project_list_url(self):
+        self.assertEqual(resolve(BASE_URL + "get-project-list").func, get_project_list)
--- universities/tests/test_models.py
+++ universities/tests/test_models.py
@@ -8,6 +8,59 @@ from django.test import TestCase
 from ..models import Major, University
 
 
+class TestUniversity(TestCase):
+    def setUp(self):
+        pass
+
+    def test_model(self):
+        now_naive = datetime.datetime.now()
+        timezone = pytz.timezone("UTC")
+        now_aware = timezone.localize(now_naive)
+
+        # test create
+        university = University.objects.create()
+        self.assertIsInstance(university, University)
+        self.assertEqual(university.pk, 1)
+        self.assertTrue(university.is_active)
+        self.assertLessEqual(now_aware, university.created_at)
+        self.assertLessEqual(now_aware, university.updated_at)
+
+        # test edit
+        name = "Test University"
+        cnpj = "XX.XXX.XXX/0001-XX"
+        university.name = name
+        university.cnpj = cnpj
+        university.is_active = False
+        university.save()
+
+        self.assertEqual(university.name, name)
+        self.assertEqual(university.cnpj, cnpj)
+        self.assertFalse(university.is_active)
+
+        # testing cpnj unique constrain
+        with transaction.atomic():
+            self.assertRaises(IntegrityError, University.objects.create, cnpj=cnpj)
+
+        # test delete
+        university.delete()
+        self.assertFalse(University.objects.filter().exists())
+
+    def test_ordering(self):
+        university01 = University.objects.create(name="aTest University")
+        university03 = University.objects.create(name="cTest University")
+        university02 = University.objects.create(name="bTest University")
+        university05 = University.objects.create(name="eTest University")
+        university04 = University.objects.create(name="dTest University")
+
+        self.assertEqual(
+            list(University.objects.all()), [university01, university02, university03, university04, university05]
+        )
+
+    def test_str(self):
+        university = University.objects.create()
+        self.assertEqual(str(university), university.name)
+
+
 class TestMajor(TestCase):
     def setUp(self):
         pass
@@ -45,53 +98,3 @@ class TestMajor(TestCase):
     def test_str(self):
         major = Major.objects.create(name="Economics")
         self.assertEqual(str(major), major.name)
-
-
-class TestUniversity(TestCase):
-    def setUp(self):
-        pass
-
-    def test_model(self):
-        now_naive = datetime.datetime.now()
-        timezone = pytz.timezone("UTC")
-        now_aware = timezone.localize(now_naive)
-
-        # test create
-        university = University.objects.create()
-        self.assertIsInstance(university, University)
-        self.assertEqual(university.pk, 1)
-        self.assertLessEqual(now_aware, university.created_at)
-        self.assertLessEqual(now_aware, university.updated_at)
-
-        # test edit
-        name = "Test University"
-        cnpj = "XX.XXX.XXX/0001-XX"
-        university.name = name
-        university.cnpj = cnpj
-        university.save()
-
-        self.assertEqual(university.name, name)
-        self.assertEqual(university.cnpj, cnpj)
-
-        # testing cpnj unique constrain
-        with transaction.atomic():
-            self.assertRaises(IntegrityError, University.objects.create, cnpj=cnpj)
-
-        # test delete
-        university.delete()
-        self.assertFalse(University.objects.filter().exists())
-
-    def test_ordering(self):
-        university01 = University.objects.create(name="aTest University")
-        university03 = University.objects.create(name="cTest University")
-        university02 = University.objects.create(name="bTest University")
-        university05 = University.objects.create(name="eTest University")
-        university04 = University.objects.create(name="dTest University")
-
-        self.assertEqual(
-            list(University.objects.all()), [university01, university02, university03, university04, university05]
-        )
-
-    def test_str(self):
-        university = University.objects.create()
-        self.assertEqual(str(university), university.name)
