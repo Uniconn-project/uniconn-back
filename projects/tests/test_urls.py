3ff355e3328279c19cb9e795565ee4c811f7e7e0
Felipe Carvalho <felipebarrettocarvalho@gmail.com>
2021-07-27 13:41:17 -0300

    updated all models tests

--- projects/tests/test_urls.py
+++ projects/tests/test_urls.py
@@ -7,8 +7,8 @@ BASE_URL = "/api/projects/"
 
 
 class TestUrls(TestCase):
-    def test_get_markets_name_list_url(self):
-        self.assertEqual(resolve(BASE_URL + "get-markets-name-list").func, get_markets_name_list)
+    def test_get_fields_name_list_url(self):
+        self.assertEqual(resolve(BASE_URL + "get-fields-name-list").func, get_fields_name_list)
 
     def test_get_projects_list_url(self):
         self.assertEqual(resolve(BASE_URL + "get-projects-list").func, get_projects_list)
