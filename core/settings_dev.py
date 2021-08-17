4514aa7afa2759240b703dc0df2e59132731e649
Felipe Carvalho <felipebarrettocarvalho@gmail.com>
2021-08-17 00:03:09 -0300

    '.'

--- core/settings_dev.py
+++ core/settings_dev.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "backend"]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
